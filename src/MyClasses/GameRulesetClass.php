ca7a232e30af7c3861424b034c8e4b2b73b8b4d8
Dmitrii Mishakov <gm.kaminari@gmail.com>
2023-05-26 01:16:40 +0300

    Calculating of results is moved to another class

--- src/MyClasses/GameRulesetClass.php
+++ src/MyClasses/GameRulesetClass.php
@@ -8,12 +8,9 @@ class GameRulesetClass
     private string $computerMove;
     private string $hmacKey;
     private array $possibleWeapons;
-    private int $weaponsQuantity;
-
     public function __construct(array $possibleWeapons)
     {
         $this->possibleWeapons = $possibleWeapons;
-        $this->weaponsQuantity = count($possibleWeapons);
         $this->generateComputerMove($this->possibleWeapons);
         $this->generateSecretKey();
         $this->buildHmacKey($this->getComputerMove(), $this->getSecretKey());
@@ -30,23 +27,7 @@ class GameRulesetClass
     {
         return $this->hmacKey;
     }
-    public function calculateResultOfGame(string $computerMove, string $playerMove): string
-    {
-        $compEval = array_search($computerMove, $this->possibleWeapons);
-        $playerEval = array_search($playerMove, $this->possibleWeapons);
-
-        // TODO: Replace placeholder below with real logic
-        // --- <Placeholder>
-        if ($compEval < $playerEval) {
-            $result = 'You lose!';
-        } else {
-            $result = 'You win!';
-        }
-        // --- </Placeholder>
-
-        return $result;
-    }
-    public function generateComputerMove($weapons): void
+    public function generateComputerMove(array $weapons): void
     {
         $this->computerMove = $weapons[array_rand($weapons)];
     }
@@ -56,7 +37,6 @@ class GameRulesetClass
     }
     public function buildHmacKey(string $computerMove, string $secretKey): void
     {
-//        echo "Computer move: " . $computerMove . PHP_EOL; echo "Secret key: " . $secretKey . "\n";
         $this->hmacKey = hash_hmac('sha3-256', $computerMove, $secretKey);
     }
 }
