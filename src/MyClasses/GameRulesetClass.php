7d3f4796ac8bd62bcd44bb70b72236392720e9c7
Dmitrii Mishakov <gm.kaminari@gmail.com>
2023-05-25 13:57:46 +0300

    Some basic logic is implemented

---
+++ src/MyClasses/GameRulesetClass.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\MyClasses;
+
+class GameRulesetClass
+{
+    private string $secretKey;
+    private string $computerMove;
+    private string $hmacKey;
+    private array $possibleWeapons;
+    private int $weaponsQuantity;
+
+    public function __construct(array $possibleWeapons)
+    {
+        $this->possibleWeapons = $possibleWeapons;
+        $this->weaponsQuantity = count($possibleWeapons);
+        $this->generateComputerMove($this->possibleWeapons);
+        $this->generateSecretKey();
+        $this->buildHmacKey($this->getComputerMove(), $this->getSecretKey());
+    }
+    public function getComputerMove(): string
+    {
+        return $this->computerMove;
+    }
+    public function getSecretKey(): string
+    {
+        return $this->secretKey;
+    }
+    public function getHmacKey(): string
+    {
+        return $this->hmacKey;
+    }
+    public function calculateResultOfGame(string $computerMove, string $playerMove): string
+    {
+        $compEval = array_search($computerMove, $this->possibleWeapons);
+        $playerEval = array_search($playerMove, $this->possibleWeapons);
+
+        // TODO: Replace placeholder below with real logic
+        // --- <Placeholder>
+        if ($compEval < $playerEval) {
+            $result = 'You lose!';
+        } else {
+            $result = 'You win!';
+        }
+        // --- </Placeholder>
+
+        return $result;
+    }
+    public function generateComputerMove($weapons): void
+    {
+        $this->computerMove = $weapons[array_rand($weapons)];
+    }
+    public function generateSecretKey(): void
+    {
+        $this->secretKey = hash('sha3-256', mt_rand());
+    }
+    public function buildHmacKey(string $computerMove, string $secretKey): void
+    {
+        echo "Computer move: " . $computerMove . PHP_EOL; echo "Secret key: " . $secretKey . "\n";
+        $this->hmacKey = hash_hmac('sha3-256', $computerMove, $secretKey);
+    }
+}
